df66a5c43c032059905252af046213fc9b0e224c
Raghavan <raghvn.r@gmail.com>
2025-12-22 12:52:53 -0500

    Update NAK Payload size to 1200 (#6)
    Signed-off-by: raghavan.rajagopalan <raghavan.rajagopalan@siden.io>
    Co-authored-by: raghavan.rajagopalan <raghavan.rajagopalan@siden.io>

--- packet/packet.go
+++ packet/packet.go
@@ -20,6 +20,7 @@ import (
 const MAX_SEQUENCENUMBER uint32 = 0b01111111_11111111_11111111_11111111
 const MAX_TIMESTAMP uint32 = 0b11111111_11111111_11111111_11111111
 const MAX_PAYLOAD_SIZE = 1456
+const MAX_NAK_PAYLOAD_SIZE = 1200
 
 // Table 1: SRT Control Packet Types
 type CtrlType uint16
@@ -1353,7 +1354,7 @@ func (c *CIFNAK) Marshal(w io.Writer) {
 			bytesWritten += 8
 		}
 
-		if bytesWritten >= MAX_PAYLOAD_SIZE-4 {
+		if bytesWritten >= MAX_NAK_PAYLOAD_SIZE {
 			break
 		}
 	}
